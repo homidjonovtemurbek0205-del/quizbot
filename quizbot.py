a20001bba2425f16bb8d2de05d7527cacd1f3ec1
homidjonovtemurbek0205-del <homidjonovtemurbek0205@gmail.com>
2025-09-07 07:43:18 +0500

    quizbot.py

--- quizbot.py
+++ quizbot.py
@@ -1,29 +1,30 @@
+import os
 import telebot
 
-# Sizning bot tokeningiz
-TOKEN = "8258584354:AAGlMeGvjNnWbQu7qNF6W3ukR9vhh3vtdcc"
+# Tokenni Environment Variables dan olish
+TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
-# Boshlash komandasi
+# Start komandasi
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(
         message.chat.id,
-        "Salom! Men quiz botman. Savol berishim uchun /quiz buyrug‘ini bosing."
+        "Salom! Men quiz botman. Savol berish uchun /quiz ni bosing."
     )
 
 # Quiz komandasi
 @bot.message_handler(commands=['quiz'])
 def quiz(message):
     question = "O‘zbekiston poytaxti qaysi?"
-    options = ["Toshkent", "Samarqand", "Buxoro", "Andijon"]
+    options = ["Toshkent", "Samarqand", "Buxoro"]
 
     bot.send_poll(
         chat_id=message.chat.id,
         question=question,
         options=options,
         type="quiz",
-        correct_option_id=0,  # 0 -> "Toshkent" to‘g‘ri javob
+        correct_option_id=0,  # 0 -> Toshkent
         is_anonymous=False
     )
 
